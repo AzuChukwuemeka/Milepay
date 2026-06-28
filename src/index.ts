fbab31fb3e65f96a03043d16373ec526007ed744
Azu Chukwuemeka <65517040+AzuChukwuemeka@users.noreply.github.com>
2026-06-28 10:37:45 +0100

    chore(update): fixing swagger issue by inserting raw HTML

--- src/index.ts
+++ src/index.ts
@@ -59,13 +59,18 @@ app.get('/docs', (_req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.send(`<!DOCTYPE html>
 <html>
-  <head>
-    <title>MilePay API</title>
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui.css">
+<head>
+  <title>MilePay API</title>
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui.css">
+  <style>
+    .swagger-ui .opblock-body { display: block !important; }
+    .swagger-ui .opblock.is-open .opblock-body { display: block !important; }
+    .swagger-ui .opblock-content { display: block !important; }
+  </style>
   </head>
-  <body>
+   <body>
     <div id="swagger-ui"></div>
     <script src="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-bundle.js" crossorigin></script>
     <script src="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-standalone-preset.js" crossorigin></script>
