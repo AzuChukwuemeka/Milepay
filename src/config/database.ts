3154cbf10913745775cc20f5cee9ce3b4703e60d
Azu Chukwuemeka <65517040+AzuChukwuemeka@users.noreply.github.com>
2026-06-28 11:20:03 +0100

    chore: updated postgres config to support vercel better

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,8 +6,8 @@ dotenv.config();
 const config: PoolConfig = {
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
-  max: 20,
-  idleTimeoutMillis: 30000,
+  max: 1,
+  idleTimeoutMillis: 10000,
   connectionTimeoutMillis: 10000,
 };
 
